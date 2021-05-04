0daf9d53f1031c7e63d4743fdefb19222acde298
SamSmith <sam@gsamsmith.com>
2021-05-03 15:55:49 -1000

    Performed first failing and passing test on object

--- tests/TwitterTest.php
+++ tests/TwitterTest.php
@@ -1,9 +1,14 @@
 <?php
 
+use \DP\Socials\Twitter;
+
 class TwitterTest extends \PHPUnit\Framework\TestCase
 {
 	public function test_shouldWork()
 	{
-		$this->assertTrue(true);
+		$twitter = new Twitter();
+		$status = "totes";
+		$twitter->tweet($status);
+		$this->assertEquals('totes', $twitter->tweet($status));
 	}
 }
